dad394c2c99e4fd261e7f5c1abf400babca8a9df
ba5eem <baseem@gmail.com>
2020-10-13 08:59:10 -0500

    project update

--- swarm.py
+++ swarm.py
@@ -4,17 +4,41 @@ import olympe
 import time
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing
 
-DRONE_IP_A = "10.202.0.1"
-DRONE_IP_B = "10.202.1.1"
+april_ip = "10.202.0.1"
+casey_ip = "10.202.1.1"
+donatello_ip = "10.202.2.1"
+leonardo_ip = "10.202.3.1"
+michelangelo_ip = "10.202.4.1"
+raphael_ip = "10.202.5.1"
+splinter_ip = "10.202.6.1"
 
 
 def main():
-    drone_A = olympe.Drone(DRONE_IP_A)
-    drone_B = olympe.Drone(DRONE_IP_B)
-    drone_A.connect()
-    drone_B.connect()
-    drone_A(TakeOff()).wait().success()
-    drone_B(TakeOff()).wait().success()
+    april = olympe.Drone(april_ip)
+    casey = olympe.Drone(casey_ip)
+    donatello = olympe.Drone(donatello_ip)
+    leonardo = olympe.Drone(leonardo_ip)
+    michelangelo = olympe.Drone(michelangelo_ip)
+    raphael = olympe.Drone(raphael_ip)
+    splinter = olympe.Drone(splinter_ip)
+
+    april.connect()
+    casey.connect()
+    donatello.connect()
+    leonardo.connect()
+    michelangelo.connect()
+    raphael.connect()
+    splinter.connect()
+
+
+    
+    april(TakeOff()).wait().success()
+    casey(TakeOff()).wait().success()
+    donatello(TakeOff()).wait().success()
+    leonardo(TakeOff()).wait().success()
+    michelangelo(TakeOff()).wait().success()
+    raphael(TakeOff()).wait().success()
+    splinter(TakeOff()).wait().success()
 
 
 if __name__ == "__main__":
