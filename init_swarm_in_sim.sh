d8cac43ae9f8e713fa4387715240b40cc8050c8b
ba5eem <baseem@gmail.com>
2020-10-13 08:34:16 -0500

    project update

--- init_swarm_in_sim.sh
+++ init_swarm_in_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH=/opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone
 
-V=1 sphinx $PATH::name=leonardo::stolen_interface=::simple_front_cam=false $PATH::name=raphael::stolen_interface=::with_front_cam=false $PATH::name=donatello::stolen_interface=::with_front_cam=false $PATH::name=michelangelo::stolen_interface=::with_front_cam=false $PATH::name=splinter::stolen_interface=::with_front_cam=false $PATH::name=april::stolen_interface=::with_front_cam=false $PATH::name=casey::stolen_interface=::with_front_cam=false::pose="0 0 0 1 0 0 2 0 0 3 0 0 4 0 0 5 0 0 6 0 0"
+
+V=1 sphinx /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=leonardo::stolen_interface=::simple_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=raphael::stolen_interface=::with_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=donatello::stolen_interface=::with_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=michelangelo::stolen_interface=::with_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=splinter::stolen_interface=::with_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=april::stolen_interface=::with_front_cam=false /opt/parrot-sphinx/usr/share/sphinx/drones/anafi4k.drone::name=casey::stolen_interface=::with_front_cam=false::pose="0 0 0 1 0 0 2 0 0 3 0 0 4 0 0 5 0 0 6 0 0"
 
