8b8aa252258a88adb52aa54eca7551a9f94ce805
ba5eem <baseem@gmail.com>
2020-10-13 14:50:31 -0500

    project update

---
+++ takeoff_test.py
@@ -0,0 +1,37 @@
+import math
+import olympe
+from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing, moveTo, Circle, PCMD
+from olympe.messages.ardrone3.PilotingState import moveToChanged, FlyingStateChanged, PositionChanged, AttitudeChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.PilotingState import GpsLocationChanged
+from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
+
+april_ip = "10.202.0.1" 
+casey_ip = "10.202.1.1"
+donatello_ip = "10.202.2.1"
+leonardo_ip = "10.202.3.1"
+michelangelo_ip = "10.202.4.1"
+raphael_ip = "10.202.5.1"
+splinter_ip = "10.202.6.1"
+
+april = olympe.Drone(april_ip)
+casey = olympe.Drone(casey_ip)
+donatello = olympe.Drone(donatello_ip)
+leonardo = olympe.Drone(leonardo_ip)
+michelangelo = olympe.Drone(michelangelo_ip)
+raphael = olympe.Drone(raphael_ip)
+splinter = olympe.Drone(splinter_ip)
+
+# Take-off
+# drone(TakeOff()).wait()
+
+
+swarm = [april, casey, donatello, leonardo, michelangelo, raphael, splinter]
+
+
+for drone in swarm:
+    drone.connection()
+
+
+for drone in swarm:
+    drone(TakeOff()).wait()
