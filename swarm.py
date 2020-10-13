522cda2e44fa809bc2cd923c16cbdc35245c34f5
ba5eem <baseem@gmail.com>
2020-10-13 09:09:25 -0500

    project update

--- swarm.py
+++ swarm.py
@@ -4,13 +4,22 @@ import olympe
 import time
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing
 
-april_ip = "10.202.0.1"
+# moveBy(0, 0, 0, 0)# (a,b,c,d) a = X-axis, b = Z-axis, c = Y-axis, d = Y-axis pivot
+
+april_ip = "10.202.0.1" 
 casey_ip = "10.202.1.1"
 donatello_ip = "10.202.2.1"
 leonardo_ip = "10.202.3.1"
 michelangelo_ip = "10.202.4.1"
 raphael_ip = "10.202.5.1"
 splinter_ip = "10.202.6.1"
+
+# backwards with ' - '
+# left with ' - '
+back_forward = -2 # X
+right_left = 0 # Z
+Y = 0
+pivot = 0
 
 
 def main():
@@ -39,6 +48,17 @@ def main():
     michelangelo(TakeOff()).wait().success()
     raphael(TakeOff()).wait().success()
     splinter(TakeOff()).wait().success()
+
+
+
+    april(moveBy( back_forward , right_left , Y , pivot )).wait()
+    casey(moveBy( back_forward , right_left , Y , pivot )).wait()
+    donatello(moveBy( back_forward , right_left , Y , pivot )).wait()
+    leonardo(moveBy( back_forward , right_left , Y , pivot )).wait()
+    michelangelo(moveBy( back_forward , right_left , Y , pivot )).wait()
+    raphael(moveBy( back_forward , right_left , Y , pivot )).wait()
+    splinter(moveBy( back_forward , right_left , Y , pivot )).wait()
+    
 
 
 if __name__ == "__main__":
