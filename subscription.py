77bf3b96c3ca4766c980b5e19b0470d6f8ca0585
ba5eem <baseem@gmail.com>
2020-10-14 14:04:12 -0500

    project update

--- subscription.py
+++ subscription.py
@@ -5,7 +5,8 @@ from olympe.messages.ardrone3.PilotingState import (
     AlertStateChanged,
     FlyingStateChanged,
     NavigateHomeStateChanged,
-    moveToChanged
+    moveToChanged,
+    GpsLocationChanged
 )
 from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
 olympe.log.update_config({"loggers": {"olympe": {"level": "WARNING"}}}) #quiet the output
@@ -25,29 +26,42 @@ drone(
     | (TakeOff() & FlyingStateChanged(state="hovering"))
 ).wait()
 
-class FlightListener(olympe.EventListener):
-    @olympe.listen_event(PositionChanged())
-    def onPositionChanged(self, event, scheduler):
-        print(event.args["latitude"], event.args["longitude"])
-        moveCasey(event.args["latitude"], event.args["longitude"])
 
-
-
-
-
-def moveCasey(lat,lng):
-    casey(
-        moveTo(lat,  lng, 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0)
-        >> FlyingStateChanged(state="hovering", _timeout=5)
-        >> moveToChanged(latitude=lat, longitude=lng, altitude=0.8, orientation_mode=MoveTo_Orientation_mode.TO_TARGET, status='DONE', _policy='wait')
-        >> FlyingStateChanged(state="hovering", _timeout=5)
+drone(
+        moveBy(-5, 0, 0, 0)
+        >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        >> drone(moveBy(10, 0, 0, 0))
+        >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
     ).wait()
 
+# class FlightListener(olympe.EventListener):
+#     @olympe.listen_event(PositionChanged())
+#     def onPositionChanged(self, event, scheduler):
+#         print(event.args["latitude"], event.args["longitude"])
+#         moveCasey(event.args["latitude"], event.args["longitude"])
 
 
-with FlightListener(drone):
-    drone(moveBy(-5, 0, 0, 0)).wait()
-    drone(moveBy(10, 0, 0, 0)).wait()
+
+
+
+# def moveCasey(lat,lng):
+#     casey(
+#         moveTo(lat,  lng, 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0)
+#         >> FlyingStateChanged(state="hovering", _timeout=5)
+#         >> moveToChanged(latitude=lat, longitude=lng, altitude=0.8, orientation_mode=MoveTo_Orientation_mode.TO_TARGET, status='DONE', _policy='wait')
+#         >> FlyingStateChanged(state="hovering", _timeout=5)
+#     ).wait()
+
+
+
+# with FlightListener(drone):
+#     drone(
+#         moveBy(-5, 0, 0, 0)
+#         >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+#         >> drone(moveBy(-5, 0, 0, 0))
+#         >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+#     ).wait()
+
 
     
     # drone(Landing()).wait()
