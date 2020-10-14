ed8c79155271c7cbdab22e3f6ea6c3d24054de4c
ba5eem <baseem@gmail.com>
2020-10-14 15:17:23 -0500

    project update

---
+++ init3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+V=1 sphinx coconut.world april.drone::stolen_interface=::simple_front_cam=false casey.drone::stolen_interface=::simple_front_cam=false donatello.drone::stolen_interface=::simple_front_cam=false
+
