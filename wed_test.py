2fbefdddcc9d24d4816658996a87e75e5a23c6b5
ba5eem <baseem@gmail.com>
2020-10-14 12:42:27 -0500

    project update

---
+++ wed_test.py
@@ -0,0 +1,130 @@
+import math
+import threading
+import olympe
+from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing, moveTo, Circle, PCMD
+from olympe.messages.ardrone3.PilotingState import moveToChanged, FlyingStateChanged, PositionChanged, AttitudeChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.PilotingState import GpsLocationChanged
+from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
+
+def findOffset(lat,lng,n,e):
+    earthRadius=6378137
+    dLat = n/earthRadius
+    dLngM = earthRadius*math.cos(math.pi*lat/180)
+    dLng = e/dLngM
+    latOffset = lat + dLat * 180/math.pi
+    lngOffset = lng + dLng * 180/math.pi
+    return [latOffset,lngOffset]
+
+
+
+def makeTriangleFormation(lat,lng):
+    april = findOffset(lat,lng,20,0)
+    rapheal = findOffset(lat,lng,-40,0)
+    michaelangelo = findOffset(lat,lng,-20,20)
+    donatello = findOffset(lat,lng,-20,-20)
+    splinter = findOffset(lat,lng,-40,40)
+    leonardo = findOffset(lat,lng,-40,-40)
+    casey = findOffset(lat,lng,-70,0)
+    return [april, rapheal, michaelangelo, donatello, splinter, leonardo, casey]
+
+def makeSquareFormation(lat,lng):
+    april = findOffset(lat,lng,40,0) # leader middle
+    rapheal = findOffset(lat,lng,-40,0) # middle bottom
+    michaelangelo = findOffset(lat,lng,0,40) # top right
+    donatello = findOffset(lat,lng,0,-40) # top left
+    splinter = findOffset(lat,lng,0,0) # middle top
+    leonardo = findOffset(lat,lng,-40,-40) # bottom left
+    casey = findOffset(lat,lng,-40,40) # bottom right
+    return [april, rapheal, michaelangelo, donatello, splinter, leonardo, casey]
+
+def makeLineFormation(lat,lng):
+    # april = findOffset(lat,lng,100,0) 
+    # rapheal = findOffset(lat,lng,80,0) 
+    # michaelangelo = findOffset(lat,lng,60, 0) 
+    # donatello = findOffset(lat,lng,40,0) 
+    # splinter = findOffset(lat,lng,20,0) 
+    # leonardo = findOffset(lat,lng,0,0) 
+    casey = findOffset(lat,lng,-20,0) 
+    return casey
+
+
+# arr = makeLineFormation(21.368492831528414,-157.712818)
+# print(arr)
+
+
+
+april_ip = "10.202.0.1" 
+casey_ip = "10.202.1.1"
+# donatello_ip = "10.202.2.1"
+# leonardo_ip = "10.202.3.1"
+# michelangelo_ip = "10.202.4.1"
+# raphael_ip = "10.202.5.1"
+# splinter_ip = "10.202.6.1"
+
+april = olympe.Drone(april_ip)
+casey = olympe.Drone(casey_ip)
+# donatello = olympe.Drone(donatello_ip)
+# leonardo = olympe.Drone(leonardo_ip)
+# michelangelo = olympe.Drone(michelangelo_ip)
+# raphael = olympe.Drone(raphael_ip)
+# splinter = olympe.Drone(splinter_ip)
+
+
+
+# swarm = [april, casey, donatello, leonardo, michelangelo, raphael, splinter]
+
+swarm = [april, casey]
+
+for drone in swarm:
+    drone.connection()
+
+# Take-off
+def takeOff(drone):
+    drone(
+        FlyingStateChanged(state="hovering", _policy="check")
+        | FlyingStateChanged(state="flying", _policy="check")
+        | (
+            GPSFixStateChanged(fixed=1, _timeout=10, _policy="check_wait")
+            >> (
+                TakeOff(_no_expect=True)
+                & FlyingStateChanged(
+                    state="hovering", _timeout=10, _policy="check_wait")
+            )
+        )
+    ).wait()
+
+for drone in swarm:
+    takeOff(drone)
+
+april(
+    moveBy(10, 0, 0, math.pi)
+    >> PCMD(1, 0, 0, 0, 0, 0)
+    >> FlyingStateChanged(state="hovering", _timeout=5)
+).wait().success()
+
+
+april_location = april.get_state(GpsLocationChanged)
+
+poi = makeLineFormation(april_location["latitude"],  april_location["longitude"])
+
+casey(
+    moveTo(poi[0],  poi[1], april_location["altitude"], MoveTo_Orientation_mode.TO_TARGET, 0.0)
+    >> FlyingStateChanged(state="hovering", _timeout=5)
+    >> moveToChanged(latitude=poi[0], longitude=poi[1], altitude=april_location["altitude"], orientation_mode=MoveTo_Orientation_mode.TO_TARGET, status='DONE', _policy='wait')
+    >> FlyingStateChanged(state="hovering", _timeout=5)
+).wait()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
