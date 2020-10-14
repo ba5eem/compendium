ab391f41213ea06fd030a5e9ad4217c386e8ebc6
ba5eem <baseem@gmail.com>
2020-10-14 14:08:40 -0500

    project update

--- subscription.py
+++ subscription.py
@@ -1,72 +1,141 @@
+# -*- coding: UTF-8 -*-
+
 import olympe
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy, moveTo
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy
 from olympe.messages.ardrone3.PilotingState import (
     PositionChanged,
+    SpeedChanged,
+    AttitudeChanged,
+    AltitudeAboveGroundChanged,
     AlertStateChanged,
     FlyingStateChanged,
     NavigateHomeStateChanged,
-    moveToChanged,
-    GpsLocationChanged
 )
-from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
-olympe.log.update_config({"loggers": {"olympe": {"level": "WARNING"}}}) #quiet the output
 
 
-drone = olympe.Drone("10.202.0.1")
-casey = olympe.Drone("10.202.1.1")
+olympe.log.update_config({"loggers": {"olympe": {"level": "WARNING"}}})
 
-casey.connect()
-casey(
-    FlyingStateChanged(state="hovering")
-    | (TakeOff() & FlyingStateChanged(state="hovering"))
-).wait()
-drone.connect()
-drone(
-    FlyingStateChanged(state="hovering")
-    | (TakeOff() & FlyingStateChanged(state="hovering"))
-).wait()
+DRONE_IP = "10.202.0.1"
+casey_ip = "10.202.1.1"
+casey = olympe.Drone(casey_ip)
 
-
-drone(
-        moveBy(-5, 0, 0, 0)
-        >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-        >> drone(moveBy(10, 0, 0, 0))
-        >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+def caseyTakeOff(drone):
+    drone(
+        FlyingStateChanged(state="hovering", _policy="check")
+        | FlyingStateChanged(state="flying", _policy="check")
+        | (
+            GPSFixStateChanged(fixed=1, _timeout=10, _policy="check_wait")
+            >> (
+                TakeOff(_no_expect=True)
+                & FlyingStateChanged(
+                    state="hovering", _timeout=10, _policy="check_wait")
+            )
+        )
     ).wait()
 
-# class FlightListener(olympe.EventListener):
-#     @olympe.listen_event(PositionChanged())
-#     def onPositionChanged(self, event, scheduler):
-#         print(event.args["latitude"], event.args["longitude"])
-#         moveCasey(event.args["latitude"], event.args["longitude"])
+
+def print_event(event):
+    # Here we're just serializing an event object and truncate the result if necessary
+    # before printing it.
+    if isinstance(event, olympe.ArsdkMessageEvent):
+        max_args_size = 60
+        args = str(event.args)
+        args = (args[: max_args_size - 3] + "...") if len(args) > max_args_size else args
+        print("{}({})".format(event.message.fullName, args))
+    else:
+        print(str(event))
 
 
+# This is the simplest event listener. It just exposes one
+# method that matches every event message and prints it.
+class EveryEventListener(olympe.EventListener):
+    @olympe.listen_event()
+    def onAnyEvent(self, event, scheduler):
+        print_event(event)
 
 
+# olympe.EventListener implements the visitor pattern.
+# You should use the `olympe.listen_event` decorator to
+# select the type(s) of events associated with each method
+class FlightListener(olympe.EventListener):
 
-# def moveCasey(lat,lng):
-#     casey(
-#         moveTo(lat,  lng, 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0)
-#         >> FlyingStateChanged(state="hovering", _timeout=5)
-#         >> moveToChanged(latitude=lat, longitude=lng, altitude=0.8, orientation_mode=MoveTo_Orientation_mode.TO_TARGET, status='DONE', _policy='wait')
-#         >> FlyingStateChanged(state="hovering", _timeout=5)
-#     ).wait()
+    # This set a default queue size for every listener method
+    default_queue_size = 100
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.has_observed_takeoff = False
+
+    @olympe.listen_event(PositionChanged())
+    def onPositionChanged(self, event, scheduler):
+        print(
+            "latitude = {latitude} longitude = {longitude} altitude = {altitude}".format(
+                **event.args
+            )
+        )
+
+    @olympe.listen_event(AttitudeChanged())
+    def onAttitudeChanged(self, event, scheduler):
+        print("roll = {roll} pitch = {pitch} yaw = {yaw}".format(**event.args))
+
+    @olympe.listen_event(AltitudeAboveGroundChanged())
+    def onAltitudeAboveGroundChanged(self, event, scheduler):
+        print("height above ground = {altitude}".format(**event.args))
+
+    @olympe.listen_event(SpeedChanged())
+    def onSpeedChanged(self, event, scheduler):
+        print("speedXYZ = ({speedX}, {speedY}, {speedZ})".format(**event.args))
+
+    # You can also handle multiple message types with the same method
+    @olympe.listen_event(
+        FlyingStateChanged() | AlertStateChanged() | NavigateHomeStateChanged()
+    )
+    def onStateChanged(self, event, scheduler):
+        # Here, since every "*StateChanged" message has a `state` argument
+        # we can handle them uniformly to print the current associated state
+        print("{} = {}".format(event.message.name, event.args["state"]))
+
+    # You can also monitor a sequence of event using the complete Olympe DSL syntax
+    @olympe.listen_event(
+        FlyingStateChanged(state="takingoff", _timeout=1.)
+        >> FlyingStateChanged(state="hovering", _timeout=5.)
+    )
+    def onTakeOff(self, event, scheduler):
+        # This method will be called once for each completed sequence of event
+        # FlyingStateChanged: motor_ramping -> takingoff -> hovering
+        caseyTakeOff(casey)
+        print("The drone has taken off!")
+        self.has_observed_takeoff = True
+
+    # The `default` listener method is only called when no other method
+    # matched the event message The `olympe.listen_event` decorator usage
+    # is optional for the default method, but you can use it to further
+    # restrict the event messages handled by this method or to limit the
+    # maximum size of it associated event queue (remember that the oldest
+    # events are dropped silently when the event queue is full).
+    @olympe.listen_event(queue_size=10)
+    def default(self, event, scheduler):
+        print_event(event)
 
 
+if __name__ == "__main__":
+    drone = olympe.Drone(DRONE_IP)
+    # Explicit subscription to every event
+    every_event_listener = EveryEventListener(drone)
+    every_event_listener.subscribe()
+    drone.connect()
+    every_event_listener.unsubscribe()
 
-# with FlightListener(drone):
-#     drone(
-#         moveBy(-5, 0, 0, 0)
-#         >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-#         >> drone(moveBy(-5, 0, 0, 0))
-#         >> casey(moveTo(drone.get_state(GpsLocationChanged)["latitude"],  drone.get_state(GpsLocationChanged)["longitude"], 0.7, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-#     ).wait()
+    # You can also subscribe/unsubscribe automatically using a with statement
+    with FlightListener(drone) as flight_listener:
+        for i in range(2):
+            assert drone(
+                FlyingStateChanged(state="hovering")
+                | (TakeOff() & FlyingStateChanged(state="hovering"))
+            ).wait().success()
+            assert drone(moveBy(10, 0, 0, 0)).wait().success()
+            drone(Landing()).wait()
+            assert drone(FlyingStateChanged(state="landed")).wait().success()
 
-
-    
-    # drone(Landing()).wait()
-    # drone(FlyingStateChanged(state="landed")).wait()
-    # drone.disconnect()
-
-
-
+    assert flight_listener.has_observed_takeoff
+    drone.disconnect()
