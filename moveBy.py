96ae970dad276a3da1575e05e188ae4f720395bd
ba5eem <baseem@gmail.com>
2020-10-16 11:39:09 -0500

    project update

--- moveBy.py
+++ moveBy.py
@@ -9,29 +9,29 @@ from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
 
 
 DRONE_IP = "10.202.0.1"
+olympe.Drone(DRONE_IP)
+# if __name__ == "__main__":
+#     with olympe.Drone(DRONE_IP) as drone:
+drone.connect()
 
-if __name__ == "__main__":
-    with olympe.Drone(DRONE_IP) as drone:
-        drone.connect()
-
-        # Start a flying action asynchronously
-        flyingAction = drone(
-            TakeOff()
-            >> FlyingStateChanged(state="hovering", _timeout=5)
-            >> moveTo(21.37313, -157.7123, 15, MoveTo_Orientation_mode.NONE, 0.0)
-            >> moveToChanged(status="DONE")
-            >> moveTo(21.37313, -157.7098, 15, MoveTo_Orientation_mode.NONE, 0.0)
-            >> moveToChanged(status="DONE")
-            >> Landing()
-        )
-
-
-
-        # Wait for the end of the flying action
-        if not flyingAction.wait().success():
-            assert False, "Cannot complete the flying action"
+# Start a flying action asynchronously
+drone(
+    TakeOff()
+    >> FlyingStateChanged(state="hovering", _timeout=5)
+    >> moveTo(21.37313, -157.7123, 15, MoveTo_Orientation_mode.NONE, 0.0)
+    >> moveToChanged(status="DONE")
+    >> moveTo(21.37313, -157.7098, 15, MoveTo_Orientation_mode.NONE, 0.0)
+    >> moveToChanged(status="DONE")
+    >> Landing()
+)
 
 
-        # Leaving the with statement scope: implicit drone.disconnect() but that
-        # is still a good idea to perform the drone disconnection explicitly
-        drone.disconnect()
+
+# Wait for the end of the flying action
+# if not flyingAction.wait().success():
+#     assert False, "Cannot complete the flying action"
+
+
+# Leaving the with statement scope: implicit drone.disconnect() but that
+# is still a good idea to perform the drone disconnection explicitly
+drone.disconnect()
