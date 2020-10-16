10c93c24cb719234df88b55045fcf9a737b876f3
ba5eem <baseem@gmail.com>
2020-10-16 10:47:14 -0500

    project update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from olympe.messages.ardrone3.PilotingState import (
     AlertStateChanged,
     FlyingStateChanged,
     NavigateHomeStateChanged,
+    moveToChanged
 )
 
 olympe.log.update_config({"loggers": {"olympe": {"level": "WARNING"}}})
