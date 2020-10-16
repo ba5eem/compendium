6ef1f42afd9adb2276e578ebb87cad1177506a70
ba5eem <baseem@gmail.com>
2020-10-16 11:28:50 -0500

    project update

--- moveBy.py
+++ moveBy.py
@@ -2,10 +2,11 @@
 
 import olympe
 from olympe.messages.ardrone3.Piloting import TakeOff, moveTo, Landing
-from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
+from olympe.messages.ardrone3.PilotingState import FlyingStateChanged, moveToChanged
 from olympe.messages.camera import start_recording, stop_recording
 from olympe.messages import gimbal
 from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
+
 
 DRONE_IP = "10.202.0.1"
 
