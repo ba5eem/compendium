53a02ec689ea280f229c989e0ff865c83220d64d
ba5eem <baseem@gmail.com>
2020-10-20 10:04:19 -0500

    project update

--- tues.py
+++ tues.py
@@ -140,30 +140,34 @@ class FlightListener(olympe.EventListener):
                 >> moveTo(21.370950, -157.709998, 10, MoveTo_Orientation_mode.TO_TARGET, 0.0)
                 >> moveToChanged(status="DONE")
             )
+            donatello(moveToChanged(status="CANCELED")
+                >> moveTo(21.370950, -157.709998, 20, MoveTo_Orientation_mode.TO_TARGET, 0.0)
+                >> moveToChanged(status="DONE")
+            )
+            leonardo(moveToChanged(status="CANCELED")
+                >> moveTo(21.370950, -157.709998, 15, MoveTo_Orientation_mode.TO_TARGET, 0.0)
+                >> moveToChanged(status="DONE")
+            )
             two_lat = 21.370950
             two_lon = -157.709998
 
         
         casey_coords = findOffset(two_lat,two_lon,-5,0)
+        donatello_coords = findOffset(two_lat,two_lon,5,0)
+        leonardo_coords = findOffset(two_lat,two_lon,0,5)
+
+        michelangelo_coords = findOffset(event.args["latitude"],event.args["longitude"],0,-5)
+        raphael_coords = findOffset(event.args["latitude"],event.args["longitude"],5,5)
+        splinter_coords = findOffset(event.args["latitude"],event.args["longitude"],-5,-5)
+
+
+        
         casey(moveTo(casey_coords[0], casey_coords[1], 10, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-
-
-        
-
-        # donatello_coords = findOffset(event.args["latitude"],event.args["longitude"],5,0)
-        # leonardo_coords = findOffset(event.args["latitude"],event.args["longitude"],0,5)
-        # michelangelo_coords = findOffset(event.args["latitude"],event.args["longitude"],0,-5)
-        # raphael_coords = findOffset(event.args["latitude"],event.args["longitude"],5,5)
-        # splinter_coords = findOffset(event.args["latitude"],event.args["longitude"],-5,-5)
-
-
-        
-
-        # donatello(moveTo(donatello_coords[0], donatello_coords[1], 15, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-        # leonardo(moveTo(leonardo_coords[0], leonardo_coords[1], 20, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-        # michelangelo(moveTo(michelangelo_coords[0], michelangelo_coords[1], 25, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-        # raphael(moveTo(raphael_coords[0], raphael_coords[1], 30, MoveTo_Orientation_mode.TO_TARGET, 0.0))
-        # splinter(moveTo(splinter_coords[0], splinter_coords[1], 35, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        donatello(moveTo(donatello_coords[0], donatello_coords[1], 15, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        leonardo(moveTo(leonardo_coords[0], leonardo_coords[1], 20, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        michelangelo(moveTo(michelangelo_coords[0], michelangelo_coords[1], 25, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        raphael(moveTo(raphael_coords[0], raphael_coords[1], 30, MoveTo_Orientation_mode.TO_TARGET, 0.0))
+        splinter(moveTo(splinter_coords[0], splinter_coords[1], 35, MoveTo_Orientation_mode.TO_TARGET, 0.0))
 
 
 
@@ -204,18 +208,15 @@ class FlightListener(olympe.EventListener):
         # FlyingStateChanged: motor_ramping -> takingoff -> hovering
         followerTakeOff(casey)
 
-        # followerTakeOff(donatello)
-        # followerTakeOff(leonardo)
-        # followerTakeOff(michelangelo)
-        # followerTakeOff(raphael)
-        # followerTakeOff(splinter)
+        followerTakeOff(donatello)
+        followerTakeOff(leonardo)
+        followerTakeOff(michelangelo)
+        followerTakeOff(raphael)
+        followerTakeOff(splinter)
 
 
 
-        # casey(TakeOff())
-        # donatello(TakeOff())
-        # leonardo(TakeOff())
-        # michelangelo(TakeOff())
+ 
         print("The drone has taken off!")
         self.has_observed_takeoff = True
 
@@ -238,11 +239,11 @@ if __name__ == "__main__":
     drone.connect()
     casey.connect()
 
-    # donatello.connect()
-    # leonardo.connect()
-    # michelangelo.connect()
-    # raphael.connect()
-    # splinter.connect()
+    donatello.connect()
+    leonardo.connect()
+    michelangelo.connect()
+    raphael.connect()
+    splinter.connect()
 
     every_event_listener.unsubscribe()
 
